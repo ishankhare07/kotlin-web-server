dfad32624ee9e599a0fa636b487fdf25d2b0b5cf
Ishan Khare <ishankhare07@gmail.com>
2018-02-15 03:53:48 +0530

    implement threads
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- main.kt
+++ main.kt
@@ -38,41 +38,49 @@ class HttpRequest {
 	}
 }
 
-class ConnectionHandler {
-	var conn: Socket
-	var requestData: String
+class ConnectionHandler: Runnable {
+        var conn: Socket
+        var requestData: String
+        var t: Thread
 
-	constructor(conn: Socket) {
-		this.conn = conn
-		this.requestData = String()
-	}
+        constructor(conn: Socket) {
+                this.conn = conn
+                this.requestData = String()
+                this.t = Thread(this, conn.toString())
+                this.t.start()
+        }
 
-	fun readRequestData() {
-		var b = ByteArray(1024)
-		var inps = this.conn.getInputStream()
-		inps.read(b, 0, inps.available())
+        fun readRequestData() {
+                var b = ByteArray(1024)
+                var inps = this.conn.getInputStream()
+                inps.read(b, 0, inps.available())
 
-		this.requestData = String(b).trim()
-	}
+                this.requestData = String(b).trim()
+        }
 
-	fun getRequestObject(): HttpRequest {
-		if(this.requestData.isEmpty()) {
-			this.readRequestData()
-		}
-		var req = HttpRequest(this.requestData)
-		req.parseRequestData()
-		return req
-	}
+        fun getRequestObject(): HttpRequest {
+                if(this.requestData.isEmpty()) {
+                        this.readRequestData()
+                }
+                var req = HttpRequest(this.requestData)
+                req.parseRequestData()
+                return req
+        }
+
+        override fun run() {
+                var request = this.getRequestObject()
+                println(request)
+                println(request.type)
+                println(request.url)
+                println(request.headers)
+		this.conn.close()
+        }
 }
 
 fun main(args: Array<String>) {
 	var socket = ServerSocket(5000)
-
 	println("listening for connections on http://0.0.0.0:5000")
-	var handler = ConnectionHandler(socket.accept())
-	var request = handler.getRequestObject()
-	println(request)
-	println(request.type)
-	println(request.url)
-	println(request.headers)
+        while(true) {
+		ConnectionHandler(socket.accept())
+	}
 }
